c23bf399e636eeec27ddd84df3b37cd2c776aebc
pin <pinokara@pincontract.com>
2020-11-30 13:41:29 +0700

    use ruplacer in function

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+
+use actix_web::{web, App, HttpServer};
+
+mod handlers;
+mod replacer;
+// mod errors;
+// mod models;
+// mod schema;
+
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .route("/users", web::get().to(handlers::get_users))
+            .route("/users/{id}", web::get().to(handlers::get_user_by_id))
+            .route("/users", web::post().to(handlers::add_user))
+            .route("/users/{id}", web::delete().to(handlers::delete_user))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
