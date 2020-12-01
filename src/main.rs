a43cd506ae90f2d6644cd68fd05f70b7cbef5e52
pin <pinokara@pincontract.com>
2020-12-02 00:36:25 +0700

    update boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,76 @@
+#![allow(unused_must_use)]
+
+#[macro_use]
+extern crate actix_web;
+#[macro_use]
+extern crate log;
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate actix_rt;
+extern crate actix_cors;
+extern crate env_logger;
+extern crate serde;
+extern crate dotenv;
+extern crate futures;
+extern crate failure;
+extern crate derive_more;
+extern crate jsonwebtoken;
+extern crate uuid;
+extern crate bcrypt;
 
-use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
-use diesel::prelude::*;
-use diesel::r2d2::{self, ConnectionManager};
-
-mod auth;
-mod errors;
-mod handlers;
+mod api;
+mod config;
+mod constants;
+mod error;
+mod middleware;
 mod models;
 mod schema;
+mod services;
+mod utils;
 
-pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
-
-use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
-use actix_web_httpauth::extractors::AuthenticationError;
-use actix_web_httpauth::middleware::HttpAuthentication;
-
-async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
-    let config = req
-        .app_data::<Config>()
-        .map(|data| data.get_ref().clone())
-        .unwrap_or_else(Default::default);
-    match auth::validate_token(credentials.token()) {
-        Ok(res) => {
-            if res == true {
-                Ok(req)
-            } else {
-                Err(AuthenticationError::from(config).into())
-            }
-        }
-        Err(_) => Err(AuthenticationError::from(config).into()),
-    }
-}
+use actix_web::{http, HttpServer, App};
+use actix_service::Service;
+use futures::FutureExt;
+use std::{io, env};
+use std::default::Default;
+use actix_cors::Cors;
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+async fn main() -> io::Result<()> {
+    dotenv::dotenv().expect("Failed to read .env file");
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
 
-    // create db connection pool
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-    let pool: Pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
+    let app_host = env::var("APP_HOST").expect("APP_HOST not found.");
+    let app_port = env::var("APP_PORT").expect("APP_PORT not found.");
+    let app_url = format!("{}:{}", &app_host, &app_port);
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found.");
 
-    // Start http server
     HttpServer::new(move || {
-        let auth = HttpAuthentication::bearer(validator);
         App::new()
-            .wrap(auth)
-            .data(pool.clone())
-            .route("/users", web::get().to(handlers::get_users))
-            .route("/users/{id}", web::get().to(handlers::get_user_by_id))
-            .route("/users", web::post().to(handlers::add_user))
-            .route("/users/{id}", web::delete().to(handlers::delete_user))
-    })
-    .bind("127.0.0.1:8080")?
+            .wrap(Cors::new() // allowed_origin return access-control-allow-origin: * by default
+                // .allowed_origin("http://127.0.0.1:8080")
+                .send_wildcard()
+                .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600)
+                .finish())
+            .data(config::db::migrate_and_config_db(&db_url))
+            .wrap(actix_web::middleware::Logger::default())
+            .wrap(crate::middleware::authen_middleware::Authentication)
+            .wrap_fn(|req, srv| {
+                srv.call(req).map(|res| res)
+            })
+            .configure(config::app::config_services)
+        })
+    .bind(&app_url)?
     .run()
     .await
 }
